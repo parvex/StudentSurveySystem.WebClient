3ac2e8db9c84966bb8729197e08a53b3269cf61e
Gabriel Rębacz <gabrielrebacz@gmail.com>
2020-06-19 18:45:41 +0200

    infinite scroll and filtering

--- src/app/surveys-list/surveys-list.component.ts
+++ src/app/surveys-list/surveys-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SurveysService, SurveyDto } from '../generated-api-client';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-surveys-list',
@@ -8,13 +9,22 @@ import { SurveysService, SurveyDto } from '../generated-api-client';
 })
 export class SurveysListComponent implements OnInit {
 
+  pageSize = 20;
+  filterText = "";
+
   surveys: Array<SurveyDto>;
 
   constructor(private surveysService: SurveysService) {}
 
   ngOnInit(): void {
-    this.surveysService.surveysMySurveysGet().subscribe(data => {this.surveys = data; console.log(this.surveys);});
+    this.surveysService.surveysMySurveysGet().subscribe(data => this.surveys = data);
   }
 
+  onFilter(){
+    this.surveysService.surveysMySurveysGet(this.filterText, 0, this.pageSize).subscribe(data => this.surveys = data)
+  }
 
+  onScrollDown(){
+    this.surveysService.surveysMySurveysGet(this.filterText, this.surveys.length/this.pageSize, this.pageSize).subscribe(data => this.surveys.push(...data))
+  }
 }
