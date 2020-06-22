dbe5b74cee905e1072f68e1895370c6c2d3bcb2b
Gabriel Rębacz <gabrielrebacz@gmail.com>
2020-06-22 17:04:46 +0200

    update usos data

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Subscription } from 'rxjs';
+import { UsersService } from '../generated-api-client';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +15,7 @@ export class HeaderComponent implements OnInit {
   isAuthenticated = false;
   private userSub: Subscription;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private usersService: UsersService, private modalService: NgbModal, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
     this.userSub = this.authService.user.subscribe(user => {
@@ -26,5 +29,16 @@ export class HeaderComponent implements OnInit {
 
   ngOnDestroy() {
     this.userSub.unsubscribe();
+  }
+
+  onUpdateCoursesClick(content){
+    if(!this.authService.isAuthenticated)
+      return;
+
+    this.spinner.show();
+    this.usersService.usersUpdateUserUsosDataPut().subscribe((data) =>{
+      this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+      this.spinner.hide();
+  })
   }
 }
